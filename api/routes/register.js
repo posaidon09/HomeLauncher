8e4420c68a2717a4e2ef067fe8f6c1b71b7a6f73
posaidon09 <halohalo090909@gmail.com>
2025-04-19 14:20:45 +0100

    if this works i'm gonna kill everyone then myself

--- api/routes/register.js
+++ api/routes/register.js
@@ -5,12 +5,15 @@ export default async function handler(req, res) {
   try {
     const r = await fetch("https://home-launcher.vercel.app/sites.json");
     const sites = await r.json();
+    console.log(sites);
     const newEntry = await pb.collection("settings").create({
-      style: 1,
-      bg: "wind.png",
-      newTab: "_self",
-      columns: sites.columns,
-      terminal: sites.terminal,
+      settings: {
+        style: 1,
+        bg: "wind.png",
+        newTab: "_self",
+        columns: sites.columns,
+        terminal: sites.terminal,
+      },
     });
     return res
       .status(200)
