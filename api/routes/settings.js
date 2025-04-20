f38739a935cd12f5f5d66eaf39fa9f617a31afa4
posaidon09 <halohalo090909@gmail.com>
2025-04-20 15:41:18 +0100

    why must i suffer

--- api/routes/settings.js
+++ api/routes/settings.js
@@ -2,12 +2,17 @@ import PocketBase from "pocketbase";
 const pb = new PocketBase("https://posaidon.pockethost.io");
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST");
+  const origin = req.headers.origin || "*";
+
+  res.setHeader("Access-Control-Allow-Origin", origin);
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Vary", "Origin");
+
   if (req.method === "OPTIONS") {
-    return res.status(200).end(); // Just send OK for OPTIONS
+    return res.status(200).end();
   }
+
   const { k: key, v: value, id } = req.method === "POST" ? req.body : req.query;
   // POST METHOD
   if (req.method == "POST") {
