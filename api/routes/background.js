fc7e264fddd1d46b25dbadef2e84e88ce73c22a2
posaidon09 <halohalo090909@gmail.com>
2025-04-21 11:03:08 +0100

    testing on prod woooooo

--- api/routes/background.js
+++ api/routes/background.js
@@ -11,11 +11,11 @@ export const config = {
 };
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Vary", "Origin");
-
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
