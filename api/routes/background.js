43c3c8c3aea15888c0bab0e8344297d649b55e7f
posaidon09 <halohalo090909@gmail.com>
2025-04-21 10:59:14 +0100

    test

--- api/routes/background.js
+++ api/routes/background.js
@@ -11,9 +11,7 @@ export const config = {
 };
 
 export default async function handler(req, res) {
-  const origin = req.headers.origin || "*";
-
-  res.setHeader("Access-Control-Allow-Origin", origin);
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Vary", "Origin");
