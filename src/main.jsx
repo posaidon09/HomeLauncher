1a0e9ff8581ecd344c743c16ffc9df6b73bec7ee
posaidon09 <halohalo090909@gmail.com>
2025-04-24 16:07:24 +0100

    woops

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { ContextProvider } from "./lib/Context";
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<ContextProvider>
