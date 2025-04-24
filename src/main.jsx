6649e70bfbb841e0392244a83e248b19675a10fd
posaidon09 <halohalo090909@gmail.com>
2025-04-24 16:11:43 +0100

    fuck it, no analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { Analytics } from "@vercel/analytics/react";
 import { ContextProvider } from "./lib/Context";
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<ContextProvider>
 		<App />
-		<Analytics />
 	</ContextProvider>,
 );
