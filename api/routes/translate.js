1258c1a6574d47751500692b091ce937dbfc9796
posaidon09 <halohalo090909@gmail.com>
2025-05-12 14:52:43 +0100

    fix to api

--- api/routes/translate.js
+++ api/routes/translate.js
@@ -2,7 +2,7 @@ import translate from "translate";
 
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   const { text, from, to } = req.body;
   if (!text || !from || !to)
